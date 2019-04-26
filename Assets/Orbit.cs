722ed68def2f6f3b9fa0cc83ef1f2f4bb13a41e6
Jacob Baker <jacobmbaker@me.com>
2019-04-26 12:49:26 -0400

    Project Population

---
+++ Assets/Orbit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    public float camSensitivity = 4.0f;
+    public Transform player;
+
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start() {
+        offset = new Vector3(player.position.x, player.position.y + 8.0f, player.position.z + 7.0f);
+    }
+
+    // Update is called once per frame
+    void LateUpdate(){
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * camSensitivity, Vector3.up) * offset;
+        transform.position = player.position + offset;
+        transform.LookAt(player.position);
+    }
+}
---
+++ Assets/Scenes/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    // Start is called before the first frame update
+
+    private Animator anim;
+    private CharacterController controller;
+    public float moveSpeed = 20.0f;
+    public float turnSpeed = 60.0f;
+    public Vector3 moveDirection = Vector3.zero;
+    public float gravity = 20.0f;
+
+    void Start() {
+        anim = gameObject.GetComponentInChildren<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(Input.GetKey("w")||Input.GetKey("up")){
+		    anim.SetInteger("Anim", 1);
+	    } else {
+		    anim.SetInteger("Anim", 0);
+	    }
+
+        if(controller.isGrounded){
+            moveDirection = transform.forward * Input.GetAxis("Vertical") * moveSpeed;
+        }
+
+        float turn = Input.GetAxis("Horizontal");
+        transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
+        controller.Move(moveDirection * Time.deltaTime);
+        moveDirection.y -= gravity * Time.deltaTime;
+    }
+}
