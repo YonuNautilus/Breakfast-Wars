a6435bb07b3c290c58da0b9bd1e30095f39d2549
Jacob <jacobmbaker@me.com>
2019-07-09 17:57:17 -0400

    animation editing method seems good, but I want animations that move the xz position (to get better foot placement so the character actually looks like they're walking and not floating.

--- Assets/Scripts/Utilities/Helper.cs
+++ Assets/Scripts/Utilities/Helper.cs
@@ -11,7 +11,13 @@ namespace SA {
         public bool playAnim;
 
         public string[] oh_attacks;
+        public string jumpAction;
 
+        public bool doJump;
+        public bool pickupItem;
+        public bool pickupWeapon;
+        public bool poke;
+        public bool interacting;
         public int weaponType;
 
         public bool enableRootMotion;
@@ -26,8 +32,35 @@ namespace SA {
             enableRootMotion = !anim.GetBool("canMove");
             anim.applyRootMotion = enableRootMotion;
 
+            interacting = anim.GetBool("interacting");
+
             if (enableRootMotion)
                 return;
+
+            if (!interacting && pickupItem) {
+                anim.Play("Right_Hand_Grab");
+                pickupItem = false;
+            }
+
+            if (!interacting && pickupWeapon) {
+                anim.Play("Upper_Body_Grab");
+                pickupWeapon= false;
+            }
+
+            if(!interacting && poke) {
+                anim.Play("Left_Arm_Poke");
+                poke = false;
+            }
+
+            if (interacting) {
+                playAnim = false;
+                vertical = Mathf.Clamp(vertical, 0, 0.5f);
+            }
+
+            if (doJump) {
+                anim.Play("Jump_Charge");
+                doJump = false;
+            }
 
             anim.SetInteger("weaponType", weaponType);
 
@@ -35,7 +68,8 @@ namespace SA {
                 string targetAnim = "";
                 switch (weaponType) {
                     case 0:
-
+                        //targetAnim = jumpAction;
+                        vertical = 0;
                         break;
                     case 1:
                         int r = Random.Range(0, oh_attacks.Length);
@@ -43,7 +77,6 @@ namespace SA {
                         break;
                 }
 
-                vertical = 0;
                 anim.CrossFade(targetAnim, 0.2f);
 
                 //anim.SetBool("canMove", false);
@@ -52,6 +85,8 @@ namespace SA {
                 playAnim = false;
             }
             anim.SetFloat("vertical", vertical);
+
+            doJump = false;
         }
     }
 }
